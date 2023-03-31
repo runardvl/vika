8d12a8404d1970e910eed79ee69ee7e78e54ce95
runardev <runarshakirov@gmail.com>
2023-03-31 14:49:25 +0300

    scrooll to top after btn click

--- js/script.js
+++ js/script.js
@@ -9,28 +9,37 @@ $(function() {
     let btn2 = $("#btn2")
     let btn3 = $("#btn3")
     let reset = $("#reset")
+    const $scrollToTopButton = $('.scrollToTop');
 
     second.hide()
     third.hide()
     last.hide()
 
+    function scrillToTop() {
+        $('html, body').animate({scrollTop : 0},200);
+    }
+
     btn1.on('click', function () {
         first.hide()
         second.show()
+        scrillToTop()
     })
 
     btn2.on('click', function () {
         second.hide()
         third.show()
+        scrillToTop()
     })
 
     btn3.on('click', function () {
         third.hide()
         last.show()
+        scrillToTop()
     })
 
     reset.on('click', function () {
         last.hide()
         first.show()
+        scrillToTop()
     })
 });
